1075505359b1f21f43fd2ca4ea58bf0e40661338
arunnichi <arun.nichi@gmail.com>
2017-12-11 09:44:22 +0530

    Coaches test

---
+++ src/test/java/TestCialfoNew/InputValues.java
@@ -0,0 +1,33 @@
+package TestCialfoNew;
+
+public class InputValues {
+
+	public String clubName  = "cvbb";
+	public String club 	= "club6@mailinator.com";
+	public String ClubPassword = "Nichi123";
+	public String BatchName1  = "Second";
+	public String Batchname2 = "dssdf";
+
+	public String Swimmer1EmailID = "30@mailinator.com";
+	public String Swimmer1Name = "Nithish Kumar";
+	public String Swimmer2EmailID = "34@mailinator.com";
+	public String Swimmer2Name = "Jijesh Kumar";
+	public String Coach1Name = "Auto Coach3";
+	public String Coach1EmailID = "autocoach3@mailinator.com";
+	public String Coach2Name = "Meera Vasumdev";
+	public String Coach2EmailID = "18@mailinator.com";
+	public String UpApprovedCoach = "ddsfdsfd@mailinator.com";
+	public String UnApprovedSwimmer = "";
+	public String PublicGroups = "";
+	public String AllCoachesGroup = "";
+	public String Personalgroup = "";	
+	public String MessageTOAllGroupsandCoaches = "Message to All groups and Coaches from Club";
+	public String MessageTOSwimmerandCoach  = "Message to Swimmer and Coach from Club";
+	public String MessageFromSwimmer = "Message From Swimmer";
+	public String MessageFromCoach = "Message From Coach";
+	public String QuickMessageFromSwimmer = "Quick Message From Swimmer";
+	public String QuickMessageFromCoach = "Quick Message From Coach";
+	public String QuickMessageToAllGroupsandCoaches = "Quick Message to All groups and Coaches from Club";
+	public String QuickMessageToSwimmerandCoach    = "Quick Message to Swimmer and Coach from Club";
+	
+}
